8506a24b176de070f4a09f3a1f8318a2a134028d
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-07 15:05:23 +0900

    Update heading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import './Header.css';
+import {SubHeading} from '../../components'
+import {images} from '../../constants'
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className="app__wrapper_info">
+      <SubHeading />
+      <h1 className='app__header-h1'>The Key to Find Dining</h1>
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
