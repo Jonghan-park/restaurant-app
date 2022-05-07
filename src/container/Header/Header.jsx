a61a5d59ee59202222c153c84f145d46baf5d034
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-07 15:14:16 +0900

    Update Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,9 @@ import {images} from '../../constants'
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title='Chase the new flavor' />
       <h1 className='app__header-h1'>The Key to Find Dining</h1>
+      <p className="p__opensans" style={{ margin:'2rem 0' }}></p>
     </div>
     <div className="app__wrapper_img">
 
