60a2dbc4f64ddcb203812ef0e2259a14a9a112af
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-08 03:42:16 +0900

    Update aboutus

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { images } from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About us</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img' />
+        <p className="p__opensans">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.</p>
+      </div>
+    </div>
+
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,10 +9,11 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title='Chase the new flavor' />
       <h1 className='app__header-h1'>The Key to Find Dining</h1>
-      <p className="p__opensans" style={{ margin:'2rem 0' }}></p>
+      <p className="p__opensans" style={{ margin:'2rem 0' }}>If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.</p>
+      <button type='button' className='custom__button'>Explore Munu</button>    
     </div>
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
