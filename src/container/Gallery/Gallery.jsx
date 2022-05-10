27242ced3beec9e68247e7abb9719a5dec6bdb9f
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-11 08:08:44 +0900

    Update gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import {SubHeading} from '../../components'
+import {images} from '../../constants'
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
+const Gallery = () => {
+
+return (
+  <div className='app__gallery flex__center'>
+    <div className="app__gallery-content">
+      <SubHeading title='Instagram' />
+      <h1 className="headtext__cormorant">Photo Gallery</h1>
+      <p className="p__opensans" style={{color:'#AAA', marginTop:'2rem'}} ></p>
+    </div>
   </div>
 );
+}
 
 export default Gallery;
