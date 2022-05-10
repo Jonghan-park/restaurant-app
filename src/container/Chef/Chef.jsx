4768392b67ac9b94099b747a695d0262a2bc3fd7
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-10 10:55:56 +0900

    Update chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -12,6 +12,21 @@ const Chef = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chef's word" />
       <h1 className="headtext__cormorant">What we believe in</h1>
+    
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.</p>
+        </div>
+        <p className="p__opensans">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.</p>
+      </div>
+
+  <div className="app__chef-sign">
+    <p>Kevin Luo</p>
+    <p className="p__opensans">Chef & Founder</p>
+    <img src={images.sign} alt="sign" />
+  </div>
+
     </div>
   </div>
 );
