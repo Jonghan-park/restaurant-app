389ba4393976bb1d94c84e73951ab920db9dacc7
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-16 07:05:12 +0900

    Update findus

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import {SubHeading} from '../../components'
+import {images} from '../../constants'
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className="app__wrapper_info">
+      <SubHeading title='Contact' />
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.</p>
+        <p className="p__cormorant" style={{color:'#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className="p__opensans"></p>
+        <p className="p__opensans"></p>
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
